a349f98a47244edf5583ec3b8b8363de3c48ad68
christian lazo <kenishi.cristianxd@gmail.com>
2022-01-16 16:03:15 -0500

    navbar-hamburguesa

--- src/app/templates/navbar/navbar.component.ts
+++ src/app/templates/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,11 +7,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private el: ElementRef) {}
 
   public buttonClick(fragment: string): void {
     this.router.navigateByUrl('#' + fragment);
   }
 
   ngOnInit(): void {}
+
+  responsiveNav() {
+    let menu = this.el.nativeElement.querySelector('#menu-btn');
+    let navbar = this.el.nativeElement.querySelector('.header .navbar');
+    if (menu.classList.value === 'fa fa-bars') {
+      console.log('entro');
+      menu.classList.remove('fa-bars');
+      menu.classList.add('fa-times');
+    } else if (menu.classList.value === 'fa fa-times') {
+      menu.classList.remove('fa-times');
+      menu.classList.add('fa-bars');
+    }
+    navbar.classList.toggle('active');
+  }
 }
